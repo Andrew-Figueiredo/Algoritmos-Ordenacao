2809cf3308850709cca927a86a699662ef7184a2
Andrew Figueiredo <andrewdw18@gmail.com>
2020-03-16 19:18:41 -0300

    Mudanças no RAdix Sort, terminado

--- Radix_Sort/radixSort.py
+++ Radix_Sort/radixSort.py
@@ -1,23 +1,39 @@
-from Counting_Sort.countingSort import countingSort
 
-def separacao(x):
-    teste = []
-    i = 1
-    p = 0
-    while(p!=len(str(x))):
-        olha = x // i % 10
-        teste.append(olha)
-        i *= 10
-        p += 1
+def countingSort(vetor, expoente):
+    n = len(vetor)
+    counter = [0]*(10)
     
-    return teste.reverse()
+    for i in range(0,n):
+        index = (vetor[i]//expoente)
+        counter[(index)%10 ] += 1
 
+    acumulado = [0]*(n+1)
 
-def radixSort(A):
-    pass
+    for i in range(1,10):
+        counter[i] += counter[i-1]
 
-    
+    i = n-1
+    while i >= 0:
+        aux = (vetor[i]//expoente)
+        acumulado[counter[ (aux)%10 ] - 1] = vetor[i]
+        counter[(aux)%10] -= 1
+        i-=1
+    vetor[:n] = acumulado[:n]
+
+def radixSort(vetor):
+    maior = max(vetor)
+    expoente = 1
+
+    while (maior/expoente) > 0 :
+        countingSort(vetor,expoente)
+        expoente *= 10
+
 
 if __name__ == "__main__":
-    A = [222, 333, 111, 555, 444, 777, 666 ]
+
+    vetor = [170, 45, 75, 90, 802, 24, 2, 66]
+    print("Vetor desordenado: " + str(vetor) )
+    print()
+    radixSort(vetor)
+    print("Vetor Ordenado   : " + str(vetor))
 
